a07d5ef708c3092ad5b37e0d667a24e83cf083cb
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-07 15:05:00 -0300

    refactor(ui): make hero social links dynamic
    - Replace hardcoded social icons with PHP array
    - Implement foreach loop for social media list
    - Standardize image attributes (alt text, source)

--- components/hero.php
+++ components/hero.php
@@ -1,15 +1,22 @@
- <section class="flex gap-x-3">
-   <div class="w-2/3">
-     <h1 class="text-3xl font-bold">Oi, meu nome é Rafael.</h1>
-     <p class="text-xl leading-6 mt-6">Transformando lógica em soluções reais. Aqui apresento meus projetos desenvolvidos em PHP e MySQL, demonstrando minha capacidade de estruturar sistemas web e resolver desafios técnicos com criatividade e organização.</p>
-     <ul class="flex gap-x-3 mt-3">
-       <li><a href="#" target="_blank"><img class="h-8 hover:animate-bounce" src="img/facebook.png" alt="Facebook Logo"></a></li>
-       <li><a href="#" target="_blank"><img class="h-8 hover:animate-bounce" src="img/linkedin.png" alt="Linkedin Logo"></a></li>
-       <li><a href="#" target="_blank"><img class="h-8 hover:animate-bounce" src="img/twitter.png" alt="Twitter Logo"></a></li>
-       <li><a href="#" target="_blank"><img class="h-8 hover:animate-bounce" src="img/youtube.png" alt="Youtube Logo"></a></li>
-     </ul>
-   </div>
-   <div class="w-1/3 flex item-center justify-center">
-     <img class="h-60 -mt-4 hover:animate-pulse" src="img/avatar.svg" alt="Avatar">
-   </div>
- </section>
+<?php
+$items = [
+  ['href' => '',  'alt' => 'Facebook Logo', 'src' => 'img/facebook.png'],
+  ['href' => '',  'alt' => 'Linkedin Logo', 'src' => 'img/linkedin.png'],
+  ['href' => '',  'alt' => 'Twitter Logo', 'src' => 'img/twitter.png'],
+  ['href' => '',  'alt' => 'Youtube Logo', 'src' => 'img/youtube.png'],
+]
+?>
+<section class="flex gap-x-3">
+  <div class="w-2/3">
+    <h1 class="text-3xl font-bold">Oi, meu nome é Rafael.</h1>
+    <p class="text-xl leading-6 mt-6">Transformando lógica em soluções reais. Aqui apresento meus projetos desenvolvidos em PHP e MySQL, demonstrando minha capacidade de estruturar sistemas web e resolver desafios técnicos com criatividade e organização.</p>
+    <ul class="flex gap-x-3 mt-3">
+      <?php foreach ($items as $item): ?>
+        <li><a href="<?= $item['href'] ?>" target="_blank"><img class="h-8 hover:animate-bounce" src="<?= $item['src'] ?>" alt="<?= $item['alt'] ?>"></a></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <div class="w-1/3 flex item-center justify-center">
+    <img class="h-60 -mt-4 hover:animate-pulse" src="img/avatar.svg" alt="Avatar">
+  </div>
+</section>
