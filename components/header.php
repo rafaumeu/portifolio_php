f336d07f4aeeaeb4b0dcf4544cee29377dfa8d4a
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-07 15:00:16 -0300

    refactor(ui): make header navigation dynamic
    - Replace hardcoded navigation links with PHP array
    - Implement foreach loop for rendering menu items

--- components/header.php
+++ components/header.php
@@ -1,13 +1,20 @@
+<?php
+$items = [
+  ['href' => '#projetos', 'texto' => 'Projetos'],
+  ['href' => '', 'texto' => 'GitHub'],
+  ['href' => '', 'texto' => 'Linkedin'],
+  ['href' => '', 'texto' => 'Twitter'],
+];
+?>
 <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
   <div class="font-bold font-mono text-cyan-600">
     🐧Meu Portfólio
   </div>
   <div>
     <ul class="flex gap-x-3 text-gray-200 font-medium">
-      <li><a href="#projetos" class="hover:underline">Projetos</a></li>
-      <li><a href="#" class="hover:underline">GitHub</a></li>
-      <li><a href="#" class="hover:underline">Linkedin</a></li>
-      <li><a href="#" class="hover:underline">Twitter</a></li>
+      <?php foreach ($items as $item): ?>
+        <li><a href="<?= $item['href'] ?>" class="hover:underline"><?= $item['texto'] ?></a></li>
+      <?php endforeach; ?>
     </ul>
   </div>
 </header>
